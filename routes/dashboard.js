c7d462e87779f0fcb0a9e6df15d86b787f6f9f31
Gopika Bhardwaj <gbhard3@uic.edu>
2021-11-17 11:38:58 -0600

    Update and Delete

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,13 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    let userInfo = req.user.username;
+    let userInfo = req.user;
     console.log(userInfo);
 
-    let userId = req.user.id;
-    console.log(userId);
-
-    res.render('dashboard', {data: userInfo, user: userId});
+    res.render('dashboard', {userInfo});
     
 });
 
